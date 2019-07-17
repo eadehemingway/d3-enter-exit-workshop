262ba52bca5c2a1e58aa8126b197c6bb7e3a095f
eadehemingway <eade.hemingway@gmail.com>
2019-07-17 14:02:20 +0300

    add colour dictionary

--- public/components/BallColorPanel.js
+++ public/components/BallColorPanel.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
 export const BallColorPanel = ({ color, changeColor }) => {
+  const colourDictionary = {
+    LightPink: 'pink',
+    DarkSalmon: 'salmon',
+    orange: 'orange',
+    coral: 'coral',
+    CornflowerBlue: 'blue'
+  };
   return (
     <div className="color-panel">
-      <p className="panelScreen">color: {color}</p>
+      <p className="panelScreen">color: {colourDictionary[color]}</p>
       <div className="button-container">
         <button className="change-color-btn" onClick={changeColor}>
           ~
